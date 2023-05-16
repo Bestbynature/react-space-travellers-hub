748c5ce3e2e31bdd47794b23628e2f4e6134bacc
Dammy <bestbynature2010@gmail.com>
2023-05-16 09:57:46 +0100

    :rocket: Render the rockets data from an API

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../redux/rockets/rocketsSlice';
+import Rocketitem from './Rocketitem';
+import { activechoose } from '../redux/missions/missionsSlice';
 
-const Rockets = () => (
-  <div>Rockets</div>
-);
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const { rockets, loading } = useSelector((store) => store.rockets);
+
+  useEffect(() => {
+    if (rockets.length === 0) {
+      dispatch(fetchRockets());
+      dispatch(activechoose(1));
+    }
+  }, [dispatch, rockets]);
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {rockets.map((rocket) => (
+        <div key={rocket.id}>
+          <Rocketitem rocket={rocket} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Rockets;
